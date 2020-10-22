2b373def0b4743c418e133c896108bbcde6bd29a
Giovanni Collica <giovannijcollica@gmail.com>
2020-10-22 14:40:50 -0400

    fix bug in end_pond_status print function and delete unecessary skeleton code

--- hw2.c
+++ hw2.c
@@ -314,7 +314,7 @@ void print_end_pond_status(pond *ponds, int numPonds)
         failfish *f = (ponds + i)->fl->head;
         printf("%d %s %d", (ponds + i)->num, (ponds + i)->name, f->num);
 
-        for(j = 0; j < ((ponds + i)->ni) - 1; j++)
+        for(j = 0; j < ((ponds + i)->thi) - 1; j++)
         {
             printf(" %d", f->next->num);
             f = f->next;
@@ -346,53 +346,3 @@ int main()
 
     return 0;
 }
-
-
-/*
-void deleteNode(struct Node* head, int key) 
-{ 
-    if (head == NULL) 
-        return; 
-  
-    // Find the required node 
-    struct Node *curr = head, *prev; 
-    while (curr->data != key) { 
-        if (curr->next == head) { 
-            printf("\nGiven node is not found"
-                   " in the list!!!"); 
-            break; 
-        } 
-  
-        prev = curr; 
-        curr = curr->next; 
-    } 
-  
-    // Check if node is only node 
-    if (curr->next == head) { 
-        head = NULL; 
-        free(curr); 
-        return; 
-    } 
-  
-    // If more than one node, check if 
-    // it is first node 
-    if (curr == head) { 
-        prev = head; 
-        while (prev->next != head) 
-            prev = prev->next; 
-        head = curr->next; 
-        prev->next = head; 
-        free(curr); 
-    } 
-  
-    // check if node is last node 
-    else if (curr->next == head) { 
-        prev->next = head; 
-        free(curr); 
-    } 
-    else { 
-        prev->next = curr->next; 
-        free(curr); 
-    } 
-} 
-*/
